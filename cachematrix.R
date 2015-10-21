b37b305841ecf17717e9e96fce99b3f42bb502ff
keona1995 <keona1995@hotmail.com>
2015-10-20 22:16:10 -0700

    Modified cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix stores a list of functions and uses lexical scoping to
+## maintain state across multiple calls to the internal functions. The purpose is to
+## simulate a scenario where a long-running and presumably expensive calculation 
+## should be executed only when necessary by caching the result and allowing queries of the
+## result at any time.
 
-## Write a short comment describing this function
+## makeCacheMatrix is the principal function that defines a list of functions and stores
+## the input matrix and the result of the calculation.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  get <- function() x
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  getinverse <- function() inv
+  setinverse <- function(calc) inv <<- calc
+  list(get = get, set = set, getinverse = getinverse, setinverse = setinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve takes as input a makeCacheMatrix and returns the result of the 
+## calculation of the inverse of a matrix. The actions the function takes
+## depends upon the internal state of the makeCacheMatrix variable. If the result
+## has already been calculated, it is returned immediately. If the result has not been
+## calculated yet, it is kicked off.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  result <- x$getinverse()
+  if(!is.null(result)) {
+    return(result)
+  }
+  mat <- x$get()
+  tinv <- solve(mat)
+  x$setinverse(tinv)
 }
